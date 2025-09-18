d83d7ca2d9ca396dbcce6947447c5cd69c676f5e
Pavilion <msugic@gmail.com>
2025-09-18 13:55:57 +0200

    Initial project setup

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do App<</title>
+
+    <link rel=stylesheet href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="todo-app">
+            <h1>To Do App</h1>
+            <form action="" class="input-area">
+                <input type="text" id="task-input" placeholder="Add new Task...">
+                <button type="submit" id=""add-task-btn></button>
+            </form>
+        </div>
+    </div>
+</body>
+<script src="style.js"></script>
+
+</html>
