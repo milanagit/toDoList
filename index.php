2e5314b59838a31c7aab74969d7ce73f3365e956
Pavilion <msugic@gmail.com>
2025-09-21 13:49:25 +0200

    html and css ajustments, added htmll and functionality for marking if the task is done or not

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do App</title>
 
-<!-- Font Awesome from https://cdnjs.com/libraries -->
-<!-- From https://fontawesome.com/icons/ you can import icons in the body code where needed -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Font Awesome from https://cdnjs.com/libraries -->
+    <!-- From https://fontawesome.com/icons/ you can import icons in the body code where needed -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel=stylesheet href="style.css">
 </head>
@@ -22,9 +22,7 @@
             </form>
             <div class="todos-container">
                 <ul id="task-list">
-                    <!-- <li>CSSnippets</li>
-                    <li>html</li>
-                    <li>css</li> -->
+                    
                 </ul>
                 <img src="images/job_done.jpg" alt="empty" class="empty-img">
             </div>
--- script.js
+++ script.js
@@ -3,8 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('task-input');
     const addTaskBtn = document.getElementById('add-task-btn');
     const taskList = document.getElementById('task-list');
+    const emptyImage = document.querySelector('.empty-img');
+    const toggleEmptyState = () => {
+        emptyImage.style.display = taskList.children.length === 0 ? 'block' : 'none';
+    }
 
-
+    
     const addTask = (event) => {
         event.preventDefault();
         const taskText = taskInput.value.trim();
@@ -13,15 +17,27 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         const li = document.createElement('li');
-        li.textContent = taskText;
+        li.innerHTML = '<span class="material-symbols-outlined checkbox">check_box_outline_blank</span><span class="task-text">' + taskText +'</span>';
         taskList.appendChild(li);
         taskInput.value = '';
+        toggleEmptyState();
     }
 
+    /* Add Task */
     addTaskBtn.addEventListener('click', addTask);
     taskInput.addEventListener('keypress', (e) => {
         if(e.key === 'Enter') {
             addTask(e);
+        }
+    });
+
+    /* Task done */
+    taskList.addEventListener('click', (event) => {
+        const clickedTask = event.target.closest('li');
+        if (clickedTask) {
+            const checkBoxStatus = clickedTask.querySelector('.checkbox');
+            checkBoxStatus.classList.toggle('checked');
+            checkBoxStatus.textContent = (checkBoxStatus.classList.contains('checked')) ?  'check_box' : 'check_box_outline_blank';
         }
     });
 
